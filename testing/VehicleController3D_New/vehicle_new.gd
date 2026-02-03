b2ff40b39d755440a9041ee8540863cf734ac627
jonathan-durbin <jongdurbin@gmail.com>
2026-02-02 23:47:33 -0500

    Get suspension working

--- testing/VehicleController3D_New/vehicle_new.gd
+++ testing/VehicleController3D_New/vehicle_new.gd
@@ -1,13 +1,119 @@
+class_name Vehicle
 extends RigidBody3D
 
 
-@onready var vehicle_wheel_br: RayCast3D = $VehicleWheelBR
-@onready var vehicle_wheel_fr: RayCast3D = $VehicleWheelFR
-@onready var vehicle_wheel_bl: RayCast3D = $VehicleWheelBL
-@onready var vehicle_wheel_fl: RayCast3D = $VehicleWheelFL
+@export var debug: bool = false
+
+## Lookup of wheels by key (F-L, F-R, etc.).
+var wheels: Dictionary[String, VehicleWheel] = {}
+## Distance between front and rear axle centers.
+var body_length: float
+## Distance between left and right wheels on an axle.
+var axle_length: float
 
 
+## Initializes wheels and derives vehicle dimensions on ready.
 func _ready() -> void:
-	for w in [vehicle_wheel_br, vehicle_wheel_bl, vehicle_wheel_fr, vehicle_wheel_fl]:
-		w.initialize()
-		
+	_initialize_wheels()
+	_calculate_body_axle_length()
+
+
+
+
+## Finds wheel nodes, assigns roles, and initializes wheel state.
+func _initialize_wheels() -> void:
+	var wheel_nodes: Array[VehicleWheel] = []
+	wheel_nodes.assign(find_children("*", "VehicleWheel"))
+
+	if wheel_nodes.is_empty():
+		push_error("No wheel nodes assigned to Vehicle.")
+		return
+
+	# Assuming the vehicle is facing in the Vector3.FORWARD (negative Z) direction, we can iterate over each wheel and determine what side it's on.
+	# (From vehicle's perspective)
+	#    Negative X -> Left of vehicle
+	#    Positive X -> Right of vehicle
+	#    Largest Z -> Furthest back
+	#    Smallest Z -> Furthest front
+	var left_wheels: Array[VehicleWheel] = []
+	var right_wheels: Array[VehicleWheel] = []
+
+	for wheel in wheel_nodes:
+		if is_zero_approx(wheel.position.x):
+			push_warning("Wheel is centered on X; defaulting to right side: %s" % wheel.name)
+		if wheel.position.x < 0.0:
+			left_wheels.append(wheel)
+		else:
+			right_wheels.append(wheel)
+
+	if left_wheels.is_empty() or right_wheels.is_empty():
+		push_error("Wheel nodes must include both left and right wheels.")
+		return
+	if left_wheels.size() < 2 or right_wheels.size() < 2:
+		push_error("Wheel nodes must include at least two wheels per side.")
+		return
+
+	left_wheels.sort_custom(func(a: VehicleWheel, b: VehicleWheel) -> bool:
+		return a.position.z < b.position.z
+	)
+	right_wheels.sort_custom(func(a: VehicleWheel, b: VehicleWheel) -> bool:
+		return a.position.z < b.position.z
+	)
+
+	# Set the four primary wheels
+	wheels["F-L"] = left_wheels.front()
+	wheels["B-L"] = left_wheels.back()
+	wheels["F-R"] = right_wheels.front()
+	wheels["B-R"] = right_wheels.back()
+
+	# Fill in the rest of the wheels
+	var wheel_num: int = 0
+	for idx in range(1, left_wheels.size() - 1):
+		wheels["M" + str(wheel_num) + "-L"] = left_wheels[idx]
+		wheel_num += 1
+	for idx in range(1, right_wheels.size() - 1):
+		wheels["M" + str(wheel_num) + "-R"] = right_wheels[idx]
+		wheel_num += 1
+
+	# Sanity check
+	if wheels.values().size() != wheel_nodes.size():
+		push_error("Wheel array size mismatch!")
+
+	# Set debug, is_powered, and other variables
+	for wheel_key in wheels.keys():
+	# 	wheels[wheel_key].debug = debug
+	# 	# Set wheel's front-back variable (and the is_powered variable)
+	# 	if "F" in wheel_key:
+	# 		if settings.drive_type == settings.DriveType.FWD or settings.drive_type == settings.DriveType.AWD:
+	# 			wheels[wheel_key].is_powered = true
+	# 		wheels[wheel_key].front_back = VehicleWheel.FrontBackType.FRONT
+	# 	elif "B" in wheel_key:
+	# 		if settings.drive_type == settings.DriveType.RWD or settings.drive_type == settings.DriveType.AWD:
+	# 			wheels[wheel_key].is_powered = true
+	# 		wheels[wheel_key].front_back = VehicleWheel.FrontBackType.BACK
+	# 	else:
+	# 		wheels[wheel_key].front_back = VehicleWheel.FrontBackType.MIDDLE
+
+	# 	# Set wheel's left-right variable
+	# 	if "R" in wheel_key:
+	# 		wheels[wheel_key].left_right = VehicleWheel.LeftRightType.RIGHT
+	# 	else:
+	# 		wheels[wheel_key].left_right = VehicleWheel.LeftRightType.LEFT
+
+		wheels[wheel_key].initialize()
+
+
+## Calculates body and axle lengths using wheel positions.
+func _calculate_body_axle_length() -> void:
+	if not wheels.has("F-R") or not wheels.has("F-L") or not wheels.has("B-R") or not wheels.has("B-L"):
+		return
+	body_length = ((
+			wheels["F-R"].global_position.distance_to(wheels["B-R"].global_position) +
+			wheels["F-L"].global_position.distance_to(wheels["B-L"].global_position)
+		) / 2.0
+	)
+	axle_length = ((
+			wheels["F-R"].global_position.distance_to(wheels["F-L"].global_position) +
+			wheels["B-R"].global_position.distance_to(wheels["B-L"].global_position)
+		) / 2.0
+	)
--- testing/VehicleController3D_New/vehicle_wheel_new.gd
+++ testing/VehicleController3D_New/vehicle_wheel_new.gd
@@ -1,89 +1,74 @@
+class_name VehicleWheel
 extends RayCast3D
 
 
-var parent: RigidBody3D
-var is_initialized: bool = false
-var total_forces: Vector3 = Vector3.ZERO
-## Initial position of the wheel. Suspension is trying to get back to this point
-var wheel_rest_pos: Vector3
-## How much the spring has been displaced
-var spring_disp: float
-var wheel_radius: float = 0.3
-var global_collision_pos: Vector3
-var spring_strength: float = 100.0
-var previous_spring_compression: float = 0.0
-var spring_damp: float = 50.0
-var wheel_velocity: Vector3
+@export var spring_strength: float = 2000.0
+@export var spring_rest_dist: float = 0.5:
+	set(value):
+		spring_rest_dist = value
+		update_rest_dist() 
+@export var wheel_radius: float = 0.3
 
 
-@onready var wheel_mesh: MeshInstance3D = $WheelMesh
-@onready var top_marker: Marker3D = $TopMarker
-@onready var bottom_marker: Marker3D = $BottomMarker
+var debug: bool
+var parent: Vehicle
+var global_contact_point: Vector3
+var total_forces: Vector3
+var wheel_mesh: MeshInstance3D
+## damp_coefficient * (2.0 * sqrt(spring_strength * mass)).
+## Where damp_coefficient is between 0 and 1. 
+## More arcade-y: b/w 0.1 and 0.2. More realistic: b/w 0.2 and 1.0.
+## Where the wheel mesh rests relative to the origin of the raycast, downwards.
+## Calculated in initialize()
+var spring_damping: float
 
 
 func _physics_process(delta: float) -> void:
-	#if not is_initialized: 
-		#return
-	
 	total_forces = Vector3.ZERO
-	wheel_velocity = get_velocity_at(wheel_mesh.global_position)
-	if "FR" in name:
-		pass
-	
+
 	if is_colliding():
-		global_collision_pos = get_collision_point()
+		global_contact_point = get_collision_point()
+
 		_handle_suspension(delta)
-		_handle_acceleration(delta)
-		_handle_steering(delta)
-		_update_wheel_height(delta, to_local(global_collision_pos).y + wheel_radius)
-	else:
-		_update_wheel_height(delta, wheel_rest_pos.y)
+
+
+func _handle_suspension(delta: float) -> void:
+	var spring_up_dir: Vector3 = global_basis.y
+	var spring_len: float = global_position.distance_to(global_contact_point) - wheel_radius
+	var offset: float = spring_rest_dist - spring_len
+	
+	wheel_mesh.position.y = lerpf(wheel_mesh.position.y, -spring_len, delta * 20.0)
+
+	var spring_force: float = spring_strength * offset
+	
+	var world_vel: Vector3 = _get_point_velocity(global_contact_point)
+	var relative_vel: float = spring_up_dir.dot(world_vel)
+	var damping_force: float = spring_damping * relative_vel
+	
+	total_forces.y = spring_force - damping_force
+	
+	var force_vector: Vector3 = total_forces.y * spring_up_dir
+	var force_offset_pos: Vector3 = global_contact_point - parent.global_position
+	
+	parent.apply_force(force_vector, force_offset_pos)
 
 
 func initialize() -> void:
 	parent = get_parent()
-	wheel_rest_pos = wheel_mesh.position
-	is_initialized = true
 	
-
-func _handle_suspension(_delta: float) -> void:
-	var suspension_dir: Vector3 = global_basis.y
-	var to_hit: Vector3 = global_collision_pos - global_position
-	var spring_length: float = suspension_dir.dot(to_hit) # Project along suspension_dir
-
-	var spring_compression: float = wheel_rest_pos.y - spring_length
-
-	#var spring_velocity: float = (previous_spring_compression - spring_compression) / delta
-	#previous_spring_compression = spring_compression
+	var meshes: Array[MeshInstance3D]
+	meshes.assign(find_children("*Wheel*", "MeshInstance3D"))
+	wheel_mesh = meshes[meshes.find_custom(func(x: MeshInstance3D): return x.name.contains("Wheel"))]
 	
-	var vel: float = suspension_dir.dot(wheel_velocity)
-
-	#var damp_force: float = spring_damp * spring_velocity
-	#var spring_force: float = spring_strength * spring_compression
+	spring_damping = 0.1 * (2.0 * sqrt(spring_strength * parent.mass))
 	
-	total_forces.y = (spring_strength * spring_compression) + (spring_damp * vel)
-	
-	parent.apply_force(
-		total_forces.y * suspension_dir, 
-		wheel_mesh.global_position - parent.global_position
-	)
-	
-	
-func _handle_acceleration(delta: float) -> void:
-	pass
-	
-	
-func _handle_steering(delta: float) -> void:
-	pass
-	
-	
-func _update_wheel_height(delta: float, height: float) -> void:
-	wheel_mesh.position.y = lerpf(wheel_mesh.position.y, height, delta * 10.0)
+	update_rest_dist()
 
 
-## Calls PhysicsDirectBodyState3D.get_velocity_at_local_position(parent.to_local(point))
-##    to get the velocity at the position of a global position, `point`.
-func get_velocity_at(point: Vector3) -> Vector3:
-	var vehicle_direct_state: PhysicsDirectBodyState3D = PhysicsServer3D.body_get_direct_state(parent.get_rid())
-	var velocity_at_point: Vector3 = vehicle_direct_state.get_velocity_at_local_position(parent.to_local(point))
-	return velocity_at_point
+func update_rest_dist() -> void:
+	target_position.y = -(spring_rest_dist + wheel_radius)
+	wheel_mesh.position.y = -spring_rest_dist
+
+
+func _get_point_velocity(point: Vector3) -> Vector3:
+	return parent.linear_velocity + parent.angular_velocity.cross(point - parent.global_position)
