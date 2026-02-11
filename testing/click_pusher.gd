17dfea206ae29b3af5eb813d012bba1e35d9bc03
jonathan-durbin <jongdurbin@gmail.com>
2026-02-11 01:16:32 -0500

    update click pusher with better logic

--- testing/click_pusher.gd
+++ testing/click_pusher.gd
@@ -1,6 +1,11 @@
 extends Node
+class_name ClickPusher
+## Allows clicking to push RigidBody3D's
 
-@export var exclude: Array[CollisionObject3D] = []
+
+@export_range(-10.0, 10.0, 0.1) var impulse_magnitude: float = 5.0
+@export var exclude_nodes: Array[CollisionObject3D] = []
+@export_flags_3d_physics var layers: int
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -16,16 +21,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		var state: PhysicsDirectSpaceState3D = camera.get_world_3d().direct_space_state
 		var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(
 			origin,
-			origin + (normal * 1000.0)
+			origin + (normal * 1000.0),
+			layers
 		)
 		var to_exclude: Array[RID] = []
 
-		for i in exclude:
+		for i in exclude_nodes:
 			if is_instance_valid(i):
-				for c in i.find_children("*", "CollisionObject3D") + [i]:
-					to_exclude.append(c.get_rid())
+				#for c in i.find_children("*", "CollisionObject3D", false) + [i]:
+				to_exclude.append(i.get_rid())
 		query.exclude = to_exclude
 
 		var res: Dictionary = state.intersect_ray(query)
 		if res.has("collider") and res.collider is RigidBody3D:
-			res.collider.apply_impulse(normal * 5.0, res.position - res.collider.global_position)
+			res.collider.apply_impulse(normal * impulse_magnitude, res.position - res.collider.global_position)
