3227edeb4f4a5effd5aa8d0323feb4c3c53f12e8
jonathan-durbin <jongdurbin@gmail.com>
2026-02-10 22:08:24 -0500

    Move test script to "prod", the addon dir and remove scripts from test dir

--- addons/VehicleController3D/src/vehicle.gd
+++ addons/VehicleController3D/src/vehicle.gd
@@ -3,119 +3,150 @@ extends RigidBody3D
 ## RigidBody-based vehicle that coordinates wheel physics and input.
 
 
-## Toggles wheel debug rendering and force gizmos.
+enum DriveType {AWD, FWD, RWD}
+
+@export var global_ctx: GUIDEMappingContext # TODO: handle in InputManager global
+
 @export var debug: bool = false:
 	set(value):
 		debug = value
-		if wheels:
-			for wheel in wheels.keys():
-				wheels[wheel].debug = debug
+		for wheel in wheels.values():
+			if is_instance_valid(wheel):
+				wheel.debug = debug
 
-@export_group("Settings")
-#@export var input_vehicle_acceleration: String
-#@export var input_vehicle_steering: String
-#@export var settings: VehicleSettings
-## GUIDE action used for throttle input.
-@export var input_vehicle_acceleration: GUIDEAction
-## GUIDE action used for steering input.
-@export var input_vehicle_steering: GUIDEAction
-## Shared vehicle settings resource.
-@export var settings: VehicleControllerSettings
+@export var wheel_radius_map: Dictionary[VehicleController3DWheel.FrontBackType, float] = {
+	VehicleController3DWheel.FrontBackType.FRONT: 0.3,
+	VehicleController3DWheel.FrontBackType.BACK: 0.3,
+	VehicleController3DWheel.FrontBackType.MIDDLE: 0.3,
+}
 
-#@export_group("Nodes")
-#@export var wheel_nodes: Array[VehicleController3DWheel] = []
-# @export var front_right: VehicleController3DWheel
-# @export var front_left: VehicleController3DWheel
-# @export var back_right: VehicleController3DWheel
-# @export var back_left: VehicleController3DWheel
+## How much the raycast should over-extend below the ground.
+## Setting this above zero will pull the vehicle towards the ground.
+@export var over_extend_map: Dictionary[VehicleController3DWheel.FrontBackType, float] = {
+	VehicleController3DWheel.FrontBackType.FRONT: 0.1,
+	VehicleController3DWheel.FrontBackType.BACK: 0.1,
+	VehicleController3DWheel.FrontBackType.MIDDLE: 0.1,
+}
+
+@export var spring_strength_map: Dictionary[VehicleController3DWheel.FrontBackType, float] = {
+	VehicleController3DWheel.FrontBackType.FRONT: 2000.0,
+	VehicleController3DWheel.FrontBackType.BACK: 2000.0,
+	VehicleController3DWheel.FrontBackType.MIDDLE: 2000.0,
+}
+
+## How far down the raycast should the wheel mesh rest when the raycast is not colliding.
+@export var spring_rest_dist_map: Dictionary[VehicleController3DWheel.FrontBackType, float] = {
+	VehicleController3DWheel.FrontBackType.FRONT: 0.5,
+	VehicleController3DWheel.FrontBackType.BACK: 0.5,
+	VehicleController3DWheel.FrontBackType.MIDDLE: 0.5,
+}
+
+## spring_damp_coefficient * (2.0 * sqrt(spring_strength * mass)).
+## Where spring_damp_coefficient is between 0 and 1.
+## More arcade-y: b/w 0.1 and 0.2. More realistic: b/w 0.2 and 1.0.
+## Where the wheel mesh rests relative to the origin of the raycast, downwards.
+## Actual spring damping is calculated in VehicleController3DWheel.initialize()
+@export_range(0.0, 1.0, 0.001) var spring_damp_coefficient: float = 0.1:
+	set(value):
+		spring_damp_coefficient = value
+		for wheel in wheels.values():
+			if is_instance_valid(wheel):
+				wheel.calculate_spring_damping()
+
+@export_range(0.0, 1000.0, 1.0) var engine_power: float = 300.0
+@export var drive_type: DriveType = DriveType.RWD
+@export var max_speed: float = 10.0 # m/s
+@export var engine_power_curve: Curve
+
+@export var tire_turn_speed_deg: float = 50.0
+@export var tire_max_turn_deg: float = 25.0
+@export var grip_curve_map: Dictionary[VehicleController3DWheel.FrontBackType, Curve] = {
+	VehicleController3DWheel.FrontBackType.FRONT: null,
+	VehicleController3DWheel.FrontBackType.BACK: null,
+	VehicleController3DWheel.FrontBackType.MIDDLE: null,
+}
+
+@export var input_accelerate: GUIDEAction
+@export var input_steer: GUIDEAction
+@export var input_handbrake: GUIDEAction
+@export_flags_3d_physics var ground_layer: int
 
 
 ## Lookup of wheels by key (F-L, F-R, etc.).
+## If there are any middle wheels, those would be M1-R, M1-L, etc. In order from front to back.
 var wheels: Dictionary[String, VehicleController3DWheel] = {}
 ## Distance between front and rear axle centers.
 var body_length: float
 ## Distance between left and right wheels on an axle.
 var axle_length: float
-## Linear velocity expressed in vehicle-local space.
-var local_velocity: Vector3
-## Tracks whether the initial ground position has been applied.
-var initial_position_set: bool = false
+var grounded: bool = false
 
 
 ## Initializes wheels and derives vehicle dimensions on ready.
 func _ready() -> void:
+	GUIDE.enable_mapping_context(global_ctx)
 	_initialize_wheels()
 	_calculate_body_axle_length()
 
 
-## Handles input-driven steering updates each frame.
 func _physics_process(delta: float) -> void:
-	if not _has_required_inputs():
-		return
-	if not initial_position_set:
-		_try_set_initial_position()
-		return
+	_update_center_of_mass()
+	_handle_steering_rotation(delta)
 
-	# Take the vehicle's rotation out of its velocity.
-	# For example, if local_velocity.x is positive, the vehicle is moving right. If negative, it's moving left.
-	#local_velocity = global_transform.basis.inverse() * linear_velocity
 
-	# Between steering_angle and -steering_angle (before converting to radians)
-	# Negate because positive Y rotation is counter-clockwise by default
-	var steering_rotation = deg_to_rad(-input_vehicle_steering.value_axis_1d * settings.steering_angle) # TODO: express the negative in GUIDE instead of here(?)
-	if not is_zero_approx(steering_rotation):
-		wheels["F-R"].rotation.y = lerpf(
-			wheels["F-R"].rotation.y,
-			ackermann_angle(steering_rotation, 1.0),
-			settings.steering_speed*delta
+#region Physics Process
+
+func _update_center_of_mass() -> void:
+	grounded = false
+	for wheel in wheels:
+		if wheels[wheel].is_colliding():
+			grounded = true
+	center_of_mass = Vector3.ZERO if grounded else Vector3.DOWN * 0.5
+
+
+func _handle_steering_rotation(delta: float) -> void:
+	var turn_input: float = input_steer.value_axis_1d * deg_to_rad(tire_max_turn_deg)
+
+	if input_steer.is_triggered():
+		wheels["F-L"].rotation.y = (
+			# clampf(
+				lerpf(
+					wheels["F-L"].rotation.y,
+					_ackermann_angle(turn_input, 1.0),
+					deg_to_rad(tire_turn_speed_deg) * delta
+				)
+			# 	-deg_to_rad(tire_max_turn_deg), deg_to_rad(tire_max_turn_deg)
+			# )
 		)
+		wheels["F-R"].rotation.y = (
+			# clampf(
+				lerpf(
+					wheels["F-R"].rotation.y,
+					_ackermann_angle(turn_input, -1.0),
+					deg_to_rad(tire_turn_speed_deg) * delta
+				)
+			# 	-deg_to_rad(tire_max_turn_deg), deg_to_rad(tire_max_turn_deg)
+			# )
+		)
+	else:
 		wheels["F-L"].rotation.y = lerpf(
 			wheels["F-L"].rotation.y,
-			ackermann_angle(steering_rotation, -1.0),
-			settings.steering_speed*delta
+			0.0, deg_to_rad(tire_turn_speed_deg) * delta * 1.5 # Return to 0.0 slightly faster
 		)
-	else:
-		wheels["F-L"].rotation.y = lerpf(wheels["F-L"].rotation.y, 0.0, settings.steering_speed*delta*1.5) # multiply by scaling factor to return to straight faster
-		wheels["F-R"].rotation.y = lerpf(wheels["F-R"].rotation.y, 0.0, settings.steering_speed*delta*1.5)
-
-
-#region Initial Position Calculation
-
-## Attempts to snap the vehicle to the ground on first update.
-func _try_set_initial_position() -> void:
-	var start_pos: Vector3 = _query_initial_position()
-	if start_pos.is_finite():
-		global_position = start_pos
-		initial_position_set = true
-
-
-## Queries the ground beneath the vehicle to find a valid starting position.
-func _query_initial_position() -> Vector3:
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	var query := PhysicsRayQueryParameters3D.create(
-		global_position + Vector3.UP * 100.0,
-		global_position - Vector3.UP * 100.0,
-		settings.ground_layer
-	)
-	var result: Dictionary = space_state.intersect_ray(query)
-	if not result.is_empty():
-		return result.position + Vector3(0, 1, 0)
-	else:
-		return Vector3.INF
+		wheels["F-R"].rotation.y = lerpf(
+			wheels["F-R"].rotation.y,
+			0.0, deg_to_rad(tire_turn_speed_deg) * delta * 1.5
+		)
 
 #endregion
 
 
-#region Utility
+#region Util
 
 ## Calculates the Ackermann angle for a wheel on the given axle side.
-func ackermann_angle(steering_rotation: float, axle_sign: float) -> float:
+func _ackermann_angle(steering_rotation: float, axle_sign: float) -> float:
 	return atan(body_length/(body_length/tan(steering_rotation)+(axle_sign*axle_length)/2.0))
 
-#endregion
-
-
-#region Initialization
 
 ## Finds wheel nodes, assigns roles, and initializes wheel state.
 func _initialize_wheels() -> void:
@@ -150,10 +181,12 @@ func _initialize_wheels() -> void:
 		push_error("Wheel nodes must include at least two wheels per side.")
 		return
 
-	left_wheels.sort_custom(func(a: VehicleController3DWheel, b: VehicleController3DWheel) -> bool:
+	left_wheels.sort_custom(
+		func(a: VehicleController3DWheel, b: VehicleController3DWheel) -> bool:
 		return a.position.z < b.position.z
 	)
-	right_wheels.sort_custom(func(a: VehicleController3DWheel, b: VehicleController3DWheel) -> bool:
+	right_wheels.sort_custom(
+		func(a: VehicleController3DWheel, b: VehicleController3DWheel) -> bool:
 		return a.position.z < b.position.z
 	)
 
@@ -176,16 +209,16 @@ func _initialize_wheels() -> void:
 	if wheels.values().size() != wheel_nodes.size():
 		push_error("Wheel array size mismatch!")
 
-	# Set debug, is_powered, and other variables
+	# Set debug, is_powered, left/right, and front/back variables
 	for wheel_key in wheels.keys():
 		wheels[wheel_key].debug = debug
 		# Set wheel's front-back variable (and the is_powered variable)
 		if "F" in wheel_key:
-			if settings.drive_type == settings.DriveType.FWD or settings.drive_type == settings.DriveType.AWD:
+			if drive_type == DriveType.FWD or drive_type == DriveType.AWD:
 				wheels[wheel_key].is_powered = true
 			wheels[wheel_key].front_back = VehicleController3DWheel.FrontBackType.FRONT
 		elif "B" in wheel_key:
-			if settings.drive_type == settings.DriveType.RWD or settings.drive_type == settings.DriveType.AWD:
+			if drive_type == DriveType.RWD or drive_type == DriveType.AWD:
 				wheels[wheel_key].is_powered = true
 			wheels[wheel_key].front_back = VehicleController3DWheel.FrontBackType.BACK
 		else:
@@ -203,6 +236,7 @@ func _initialize_wheels() -> void:
 ## Calculates body and axle lengths using wheel positions.
 func _calculate_body_axle_length() -> void:
 	if not wheels.has("F-R") or not wheels.has("F-L") or not wheels.has("B-R") or not wheels.has("B-L"):
+		push_error("Can't calculate body axle length, at least four wheels need to be defined")
 		return
 	body_length = ((
 			wheels["F-R"].global_position.distance_to(wheels["B-R"].global_position) +
@@ -214,19 +248,5 @@ func _calculate_body_axle_length() -> void:
 			wheels["B-R"].global_position.distance_to(wheels["B-L"].global_position)
 		) / 2.0
 	)
-
-
-## Validates that required settings, inputs, and wheels are available.
-func _has_required_inputs() -> bool:
-	if settings == null:
-		push_error("Vehicle settings are missing.")
-		return false
-	if input_vehicle_acceleration == null or input_vehicle_steering == null:
-		push_error("Vehicle input actions are missing.")
-		return false
-	if not wheels.has("F-R") or not wheels.has("F-L") or not wheels.has("B-R") or not wheels.has("B-L"):
-		push_error("Vehicle wheels are not initialized.")
-		return false
-	return true
 
 #endregion
